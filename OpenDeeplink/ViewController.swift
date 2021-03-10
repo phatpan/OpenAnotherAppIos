2244c1234fa436809d900e64c74c5b48ba14c84f
Phatcharaphan Ananpreechakun <phat.pan@gmail.com>
2021-03-10 17:38:28 +0700

    Open app

--- OpenDeeplink/ViewController.swift
+++ OpenDeeplink/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
+  @IBAction func didTapOpenApp(_ sender: Any) {
+    let mobileNumber = "0846936500"
+    let clientId = "4102000587456315"
+    let scope = "profile"
+    let redirectUri = "https://redirect_uri"
+    let state = "12356700000000456178215ANM785185"
+    let responseType = "code"
+    
+    let url = URL(string: "myais://MyIds?mobileNumber=\(mobileNumber)&clientId=\(clientId)&scope=\(scope)&redirectUri=\(redirectUri)&state=\(state)&responseType=\(responseType)")
+           
+    UIApplication.shared.open(url!) { (result) in
+        if result {
 
+        }
+    }
+  }
+  
 }
 
